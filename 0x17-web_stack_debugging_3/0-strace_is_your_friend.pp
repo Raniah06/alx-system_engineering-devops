f6c7b1dc65fff0a278041eb09ed7d78be6f52b49
Raniah06 <raniafadil06@gmail.com>
2024-06-05 10:02:38 -0700

    m

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,15 @@
 # 0-strace_is_your_friend.pp
+# This Puppet manifest fixes directory permissions and notifies Apache.
 
 exec { 'fix-permissions':
   command => '/bin/chmod -R 755 /var/www/html',
   onlyif  => '/usr/bin/find /var/www/html -type d -exec /usr/bin/test ! -perm 755 \; -print | grep .',
   notify  => Service['apache2'],
+}
+
+service { 'apache2':
+  ensure    => running,
+  enable    => true,
+  subscribe => Exec['fix-permissions'],
 }
 
