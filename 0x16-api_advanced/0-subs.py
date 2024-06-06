9a988b6b0fe8242ca80686f89ce662243cae32fa
Raniah06 <raniafadil06@gmail.com>
2024-06-06 10:11:56 -0700

    Completed API advanced project

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+    
+    data = response.json().get('data', {})
+    return data.get('subscribers', 0)
+
