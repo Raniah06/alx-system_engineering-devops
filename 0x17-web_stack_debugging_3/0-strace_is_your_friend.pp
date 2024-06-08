f51c656a50f21be75e37fdf0cf496db8cb771604
Raniah06 <raniafadil06@gmail.com>
2024-06-08 06:25:02 -0700

    m

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,17 +1,8 @@
 # 0-strace_is_your_friend.pp
-# This Puppet script fixes the missing index.php file and restarts Apache
+# This Puppet manifest fixes a typo in wp-settings.php
 
-file { '/var/www/html/index.php':
-  ensure  => file,
-  content => '<?php echo "Hello, Holberton!"; ?>',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-}
-
-service { 'apache2':
-  ensure => running,
-  enable => true,
-  require => File['/var/www/html/index.php'],
+exec { 'fix-apache':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }
 
