f2ad2f49a0ae7674300732aafc161bdc7ede9bcf
Raniah06 <raniafadil06@gmail.com>
2024-06-08 10:59:53 -0700

    m

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,19 +1,8 @@
 # 0-strace_is_your_friend.pp
-# This Puppet manifest ensures the PHP module is enabled and Apache is restarted
+# This Puppet manifest fixes a typo in wp-settings.php
 
-package { 'libapache2-mod-php5':
-  ensure => installed,
-}
-
-exec { 'enable-php-module':
-  command => '/usr/sbin/a2enmod php5',
+exec { 'fix-apache2.0':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => ['/usr/bin', '/usr/sbin', '/bin'],
-  unless  => '/usr/sbin/apache2ctl -M | grep php5',
-}
-
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => [Package['libapache2-mod-php5'], Exec['enable-php-module']],
 }
 
--- 0x17-web_stack_debugging_3/1-strace_is_your_friend.pp
+++
@@ -1,8 +0,0 @@
-# 0-strace_is_your_friend.pp
-# This Puppet manifest fixes a typo in wp-settings.php
-
-exec { 'fix-apache':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => ['/usr/bin', '/usr/sbin', '/bin'],
-}
-
