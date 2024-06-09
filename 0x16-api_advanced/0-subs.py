0edaf067d050593733f4d921e83cf02d9d16a07c
Raniah06 <raniafadil06@gmail.com>
2024-06-08 21:56:41 -0700

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
+    """ GET subscriber count of a given subreddit """
+    url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'philsrequest'}
+    r = requests.get(url, headers=headers)
+    if (r.status_code is 302 or r.status_code is 404):
         return 0
-    
-    data = response.json().get('data', {})
-    return data.get('subscribers', 0)
-
+    r = r.json()
+    if ('error' in r):
+        return 0
+    elif ('subscribers' in r['data']):
+        return r['data']['subscribers']
+    else:
+        return 0
