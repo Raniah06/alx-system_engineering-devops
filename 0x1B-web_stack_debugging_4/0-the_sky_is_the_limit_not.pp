434d8324b1fc5a563b534e3d6ff287b42f9bb0d0
Raniah06 <raniafadil06@gmail.com>
2024-06-13 15:53:41 -0700

    m

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# This Puppet manifest fixes Nginx limits under high load
+exec { 'Limit':
+  command => '/usr/bin/env sed -i s/15/2000/ /etc/default/nginx',
+}
+exec { '/usr/bin/env service nginx restart': }
